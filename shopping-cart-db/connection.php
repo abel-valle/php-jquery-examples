3766001d4edbc6f2747c67a519015fadd20090df
abelvalle <abel@dsindigo.com>
2019-04-29 01:24:48 -0500

    Se quitan datos sensibles.

--- shopping-cart-db/connection.php
+++ shopping-cart-db/connection.php
@@ -5,17 +5,11 @@ function connect() {
     global $connection;
 
     // Datos del servidor MySQL.
-    /*
     $host   = "[database-host-here]";
     $db     = "[database-name-here]";
     $usr    = "[user-here]";
     $pwd    = "[pass-here]";
-    */
-    $host   = "sql202.byethost.com";
-    $db     = "b4_22610925_dci3db";
-    $usr    = "b4_22610925";
-    $pwd    = "abel12345";
-    
+
     $connection = mysqli_connect($host, $usr, $pwd, $db) or die("No se pudo conectar al servidor MySQL.");
     // Permite visualizar los acentos y caracteres especiales.
     mysqli_set_charset($connection, "utf8");
